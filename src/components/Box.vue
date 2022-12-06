d3c098e456b63e5fb9cdeeba70d8d8c5fdcdb1ee
Luís Lima <78507318+Lueasyx@users.noreply.github.com>
2022-12-06 18:12:57 -0300

    Projeto Alura Tracker

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilo">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Box',
+    data() {
+        return {
+            estilo: {
+                background: '#faf0ca',
+                margin: '1.25rem'
+            }
+        }
+    }
+})
+</script>
